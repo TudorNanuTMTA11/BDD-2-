002659c07de1e356086bdd53b80aedc95f3bdadf
TudorNanuTMTA11 <tudornanu97@gmail.com>
2025-07-01 15:09:19 +0200

    circe_hooded_ice

--- Steps/magento_circe_hooded_ice_steps.py
+++ Steps/magento_circe_hooded_ice_steps.py
@@ -2,8 +2,26 @@ from behave import *
 
 
 
+@given('I am on the Magento homepage and I want to purchase a product with a particular size and color I want')
+def step_impl(context):
+    context.circe_hooded_ice.circe_hooded_ice_page()
 
+@when('I click on Women option')
+def step_impl(context):
+    context.circe_hooded_ice.women_option()
 
+@when('I click on Tops option')
+def step_impl(context):
+    context.circe_hooded_ice.tops_option()
 
+@when('I click on Hoodies & Sweatshirts option')
+def step_impl(context):
+    context.circe_hooded_ice.hoodies_sweatshirts_option()
 
+@when('I select XS size on Circe Hooded Ice')
+def step_impl(context):
+    context.circe_hooded_ice.size_XS_circe_hooded_ice()
 
+@when('I select Gray color on Circe Hooded Ice')
+def step_impl(context):
+    context.circe_hooded_ice.color_gray_circe_hooded_ice()
