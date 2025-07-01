c841833a981c1168e697bbb7f3e6d550370e0022
TudorNanuTMTA11 <tudornanu97@gmail.com>
2025-07-01 12:46:14 +0200

    base_page

--- base_page.py
+++ base_page.py
@@ -24,9 +24,3 @@ class Base_page(Browser, Selectors_List):
             logging.error(f'An error occurred while using the last name : {str(t)}')
 
 
-    def check_error_message(self, by, selector, expected_error_message):
-                        # metoda presence_of_element_located primeste doi parametrii: tipul selectorului si valoarea selectorului
-                        error_message_web_element = WebDriverWait(self.chrome, 20).until(
-                                EC.presence_of_element_located((by, selector)))
-                        actual_error_message_text = error_message_web_element.text
-                        assert expected_error_message == actual_error_message_text, f"Error, the message is incorrect. Expected: {expected_error_message}, actual: {actual_error_message_text}"
