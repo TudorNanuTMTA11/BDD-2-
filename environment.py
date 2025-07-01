d43c8d378dfe9b3727d9595301b6be81fcfad180
TudorNanuTMTA11 <tudornanu97@gmail.com>
2025-07-01 16:27:14 +0200

    environment

--- environment.py
+++ environment.py
@@ -1,11 +1,12 @@
-from Pages.what_is_new import What_is_new
 from browser import Browser
 from base_page import Base_page
 from Pages.create_account import Create_account
 from Pages.home_page import Home_page
 from Pages.sign_in_page import Sign_in_page
+from Pages.what_is_new import What_is_new
 from Pages.checkout import Checkout
 from Pages.circe_hooded_ice import Circe_Hooded_Ice
+from Pages.tops import Tops
 
 def before_all(context):
     context.browser = Browser()
@@ -17,6 +18,7 @@ def before_all(context):
     context.what_is_new = What_is_new()
     context.checkout = Checkout()
     context.circe_hooded_ice = Circe_Hooded_Ice()
+    context.tops = Tops()
 
 def after_all(context):
     context.browser.close_browser()
