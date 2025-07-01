f60ddd6ec7802b5c41d3e444c453e8c2e24bc667
TudorNanuTMTA11 <tudornanu97@gmail.com>
2025-07-01 12:47:34 +0200

    magento_home_page_page_steps

--- Steps/magento_home_page_steps.py
+++ Steps/magento_home_page_steps.py
@@ -1,2 +1,22 @@
 from behave import *
 
+@given('I am on the Magento homepage and I want to initiate the searching products process')
+def step_impl(context):
+    context.home_page.open_home_page()
+
+@when('I type "Wayfarer Messenger Bag" in the search tab')
+def step_impl(context):
+    context.home_page.search_entire_here()
+
+@when('the options for the product searched appear')
+def step_impl(context):
+    context.home_page.wayfarer_messenger_bag_search()
+
+
+@when('I am redirected to the catalog page')
+def step_impl(context):
+    context.home_page.catalog_page()
+
+@then('I see the product shown on the page')
+def step_impl(context):
+    context.home_page.wayfarer_messenger_bag_product()
