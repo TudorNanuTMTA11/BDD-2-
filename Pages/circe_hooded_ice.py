f81321521552229b12ea90b772f64ad8bd6942ce
TudorNanuTMTA11 <tudornanu97@gmail.com>
2025-07-01 15:09:04 +0200

    circe_hooded_ice

--- Pages/circe_hooded_ice.py
+++ Pages/circe_hooded_ice.py
@@ -1,11 +1,25 @@
-from selenium.common import NoAlertPresentException
-from selenium.webdriver.common.by import By
 
-
-import logging
-
-from selenium.webdriver.support.select import Select
 
 from base_page import Base_page
 
 
+class Circe_Hooded_Ice(Base_page):
+    def size_XS_circe_hooded_ice(self):
+        self.chrome.find_element(*self.SIZE)
+
+    def color_gray_circe_hooded_ice(self):
+        self.chrome.find_element(*self.COLOR)
+
+    def women_option(self):
+        self.chrome.find_element(*self.WOMEN)
+
+    def tops_option(self):
+        self.chrome.find_element(*self.TOPS)
+
+    def hoodies_sweatshirts_option(self):
+        self.chrome.find_element(*self.HOODIES_SWEATSHIRTS)
+
+    def circe_hooded_ice_page(self):
+        page_url = "https://magento.softwaretestingboard.com/women/tops-women/hoodies-and-sweatshirts-women.html"  # am definit o variabila
+        assert page_url == page_url, (f"An error occurred. Expected page_url {page_url}",  # am scris o metoda
+                                      f"Actual current_url {page_url}")  # de verificare de tip assert"  # am definit o variabila
