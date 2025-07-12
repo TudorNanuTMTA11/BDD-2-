6d37c9cfb2851e01940d9710f5ec637d7585da6b
TudorNanuTMTA11 <tudornanu97@gmail.com>
2025-07-12 10:39:47 +0200

    hoodies_and-sweatshirts

---
+++ Pages/hoodies_and_sweatshirts.py
@@ -0,0 +1,26 @@
+from base_page import Base_page
+import logging
+
+class Hoodies_and_sweatshirts(Base_page):
+    def grid_button(self):
+        try:
+            grid_button = self.chrome.find_element(*self.GRID)
+            if grid_button:
+                grid_button.click()
+            else:
+                raise AssertionError('Grid button element was not found')
+        except Exception as i:
+            logging.error(f"An error occurred while clicking the grid button : { str(i)}")
+
+    def list_button(self):
+        try:
+            list_button = self.chrome.find_element(*self.LIST)
+            if list_button:
+                list_button.click()
+            else:
+                raise AssertionError('List button element was not found')
+        except Exception as i:
+            logging.error(f"An error occurred while clicking the list button : { str(i)}")
+
+
+
